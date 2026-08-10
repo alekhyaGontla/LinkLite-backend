2cd90b8c1eef939081aad3f80ced2f36e2ff4c16
Alekhya Gontla <alekhyag0410@gmail.com>
2026-08-10 22:45:16 +0530

    Rename UserNotFoundException to UrlNotFoundException

--- src/main/java/com/linklite/exception/UrlNotFoundException.java
+++ src/main/java/com/linklite/exception/UrlNotFoundException.java
@@ -1,7 +1,7 @@
 package com.linklite.exception;
 
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
+public class UrlNotFoundException extends RuntimeException {
+    public UrlNotFoundException(String message) {
         super(message);
     }
 }
