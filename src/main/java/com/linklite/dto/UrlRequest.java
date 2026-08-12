0983149257159c651d7259af263bcce33f334011
Alekhya Gontla <alekhyag0410@gmail.com>
2026-08-12 16:47:43 +0530

    Refactor UrlRequest class to remove duplicates
    Removed duplicate field declarations and getter/setter methods.

--- src/main/java/com/linklite/dto/UrlRequest.java
+++ src/main/java/com/linklite/dto/UrlRequest.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 public class UrlRequest {
 
-<<<<<<< HEAD
     private String originalUrl;
     private String customAlias;
     private LocalDateTime expiryDate;
@@ -33,38 +32,3 @@ public class UrlRequest {
         this.expiryDate = expiryDate;
     }
 }
-=======
-```
-private String originalUrl;
-
-private String customAlias;
-
-private LocalDateTime expiryDate;
-
-public String getOriginalUrl() {
-    return originalUrl;
-}
-
-public void setOriginalUrl(String originalUrl) {
-    this.originalUrl = originalUrl;
-}
-
-public String getCustomAlias() {
-    return customAlias;
-}
-
-public void setCustomAlias(String customAlias) {
-    this.customAlias = customAlias;
-}
-
-public LocalDateTime getExpiryDate() {
-    return expiryDate;
-}
-
-public void setExpiryDate(LocalDateTime expiryDate) {
-    this.expiryDate = expiryDate;
-}
-```
-
-}
->>>>>>> 07dc98d6affa2ec4f698256083be4284d7bddb38
