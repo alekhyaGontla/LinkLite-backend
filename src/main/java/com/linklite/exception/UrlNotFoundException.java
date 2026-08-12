773fab6b7c062fbaf659563482bc792f6fae2fb1
Alekhya Gontla <alekhyag0410@gmail.com>
2026-08-12 16:49:35 +0530

    Update UrlNotFoundException.java

--- src/main/java/com/linklite/exception/UrlNotFoundException.java
+++ src/main/java/com/linklite/exception/UrlNotFoundException.java
@@ -2,17 +2,7 @@ package com.linklite.exception;
 
 public class UrlNotFoundException extends RuntimeException {
 
-<<<<<<< HEAD
     public UrlNotFoundException(String message) {
         super(message);
     }
 }
-=======
-```
-public UrlNotFoundException(String message) {
-    super(message);
-}
-```
-
-}
->>>>>>> 07dc98d6affa2ec4f698256083be4284d7bddb38
