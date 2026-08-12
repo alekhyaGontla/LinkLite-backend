56ba4a42894813449ff33a3ac79c386361b3c8a2
Alekhya Gontla <alekhyag0410@gmail.com>
2026-08-12 16:46:34 +0530

    Update LoginRequest.java

--- src/main/java/com/linklite/dto/LoginRequest.java
+++ src/main/java/com/linklite/dto/LoginRequest.java
@@ -2,7 +2,6 @@ package com.linklite.dto;
 
 public class LoginRequest {
 
-<<<<<<< HEAD
     private String email;
     private String password;
 
@@ -22,27 +21,3 @@ public class LoginRequest {
         this.password = password;
     }
 }
-=======
-```
-private String email;
-private String password;
-
-public String getEmail() {
-    return email;
-}
-
-public void setEmail(String email) {
-    this.email = email;
-}
-
-public String getPassword() {
-    return password;
-}
-
-public void setPassword(String password) {
-    this.password = password;
-}
-```
-
-}
->>>>>>> 07dc98d6affa2ec4f698256083be4284d7bddb38
