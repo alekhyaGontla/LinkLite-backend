99e5fc5bd08f9cdc2dcc483cf5d1a363447af9b6
Alekhya Gontla <alekhyag0410@gmail.com>
2026-08-13 17:15:52 +0530

    Add HomeController with a home endpoint

---
+++ src/main/java/com/linklite/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.linklite.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "LinkLite Backend is running successfully!";
+    }
+}
